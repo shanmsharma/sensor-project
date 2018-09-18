01a0d491e339804c09f485209cddc057038513ea
shanmugasharma vijayarangan <shanmsharma@gmail.com>
2018-09-17 23:20:32 -0400

    dispaly live reading on the browser

--- hello-world/server/index.js
+++ hello-world/server/index.js
@@ -1,19 +1,27 @@
 
 
-
 const express = require('express');
-
-const app = express()
+const app = express();
+const sensor = require('node-dht-sensor');
+const getSensorReadings = require('./get-sensor-readings')
 
 app.get('/temperature',function(req,res){
-
-res.send('24 &#176C')
-});
+  getSensorReadings((err,temperature,humidity) => {
+  if(!err){
+   res.send(temperature.toFixed(1) + ' \xB0C');
+  }
+ })
+})
 
 
 app.get('/humidity', function(req,res) {
-res.send('48%');
+  getSensorReadings((err,temperature,humidity) => {
+  if(!err){
+   res.send(humidity.toFixed(1) + ' %');
+  }
+ });
 });
+
 
 app.listen(3000,function(){
 
